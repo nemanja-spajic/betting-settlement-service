dd2f8be999e6fa57dea85d0122a0e7808b17b0b3
nemanja <nemanja.spajic.extern@virtualminds.com>
2025-07-07 00:00:24 +0200

    fixed jackson config

--- src/main/java/com/sporty/betting/settlement/common/config/JacksonConfig.java
+++ src/main/java/com/sporty/betting/settlement/common/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package com.sporty.betting.settlement.common.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,8 @@ public class JacksonConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
     }
 }
