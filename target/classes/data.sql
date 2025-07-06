c7ca1d68180c4224c02cf42dead471c401fd4a75
nemanja <nemanja.spajic.extern@virtualminds.com>
2025-07-06 10:17:19 +0200

    Remove .idea and target folders from Git and add to .gitignore

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO bets (id, user_id, event_id, event_market_id, event_winner_id, bet_amount) VALUES
-   ('e3c5973a-8248-4c9c-8ec2-1c23b8c2c3d9', 'user-1', 'match-001', 'market-001', 'team-a', 50.00),
-   ('f4d8b942-3481-4cf1-84b8-dbc1184b989c', 'user-2', 'match-001', 'market-002', 'team-b', 30.00),
-   ('e876a2b5-2d8a-4f5c-9dfb-55e5a6c3b8d3', 'user-3', 'match-002', 'market-001', 'team-x', 75.00);
--- target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE bets (
-  id UUID PRIMARY KEY,
-  user_id VARCHAR(255),
-  event_id VARCHAR(255),
-  event_market_id VARCHAR(255),
-  event_winner_id VARCHAR(255),
-  bet_amount DECIMAL
-);
